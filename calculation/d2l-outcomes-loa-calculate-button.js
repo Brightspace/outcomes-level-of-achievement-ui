37b3d55afd92e9896ab4fb4ac51ef0bdf3dd2270
Callum Mitchell <mitccall@gmail.com>
2020-07-31 10:28:18 -0300

    calculate button skeleton and graphical demo page

--- calculation/d2l-outcomes-loa-calculate-button.js
+++ calculation/d2l-outcomes-loa-calculate-button.js
@@ -1,19 +1,117 @@
+/**
+`d2l-outcomes-loa-override-button`
+Polymer Web-Component for a button that lets the user manually override
+calculated values for a user's overall outcome achievement level
+
+@demo demo/d2l-outcomes-loa-override-button.html
+*/
+
+import '@polymer/polymer/polymer-legacy.js';
+import '../localize-behavior.js';
+import 'd2l-polymer-behaviors/d2l-dom.js';
+import 'd2l-typography/d2l-typography-shared-styles.js';
+import 'd2l-colors/d2l-colors.js';
+import '@brightspace-ui/core/components/tooltip/tooltip.js';
 import '@brightspace-ui/core/components/button/button-icon.js';
-import '@brightspace-ui/core/components/icons/icon.js';
 import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
 import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
 import { dom } from '@polymer/polymer/lib/legacy/polymer.dom.js';
 const $_documentContainer = document.createElement('template');
 
-$_documentContainer.innerHTML = `<dom-module id="d2l-outcomes-loa-calculation-button">
-
+$_documentContainer.innerHTML = `<dom-module id="d2l-outcomes-loa-calculate-button">
+<template strip-whitespace="">	
+	<style>
+	</style>
+	<d2l-button-icon icon="[[buttonIcon]]" id="calculateButton" aria-hidden="true" tabindex="-1" hidden="[[hidden]]">
+	</d2l-button-icon>
+</template>
 </dom-module> `;
 
+document.head.appendChild($_documentContainer.content);
 
 Polymer({
-	is: 'd2l-outcomes-loa-calculation-button',
+	is: 'd2l-outcomes-loa-override-button',
 
 	properties: {
 
+		buttonIcon: {
+			type: String,
+			value: 'tier1:calculate'
+		},
+
+		hidden: {
+			type: Boolean,
+			value: false,
+			reflectToAttribute: true
+		},
+
+		index: {
+			type: Number,
+			reflectToAttribute: true
+		},
+
+		buttonData: {
+			type: Object,
+			value: function () { return {}; }
+		},
+
+		tooltipPosition: {
+			type: String,
+			value: 'bottom',
+			reflectToAttribute: true
+		},
+	},
+
+	listeners: {
+		'keydown': '_onKeyDown',
+		'tap': '_handleTap'
+	},
+
+	behaviors: [
+		D2L.PolymerBehaviors.OutcomesLOA.LocalizeBehavior
+	],
+
+	//TODO: define event behavior and other methods
+	ready: function () {
+		afterNextRender(this, /* @this */ function () {
+		});
+	},
+
+	_keyCodes: {
+		ENTER: 13,
+		SPACE: 32
+	},
+
+	_onKeyDown: function (event) {
+		if (this.hidden) {
+			return;
+		}
+
+		if (event.keyCode === this._keyCodes.ENTER || event.keyCode === this._keyCodes.SPACE) {
+			this._handleSelected();
+			event.preventDefault();
+		}
+	},
+
+	_handleTap: function (event) {
+		if (this.hidden) {
+			return;
+		}
+
+		this._handleSelected();
+		event.preventDefault();
+	},
+
+	//Invoked when the button is clicked, tapped, or selected with the keyboard
+	_handleSelected: function () {
+		
+	},
+
+	_dispatchItemToggledEvent: function (newOverrideState) {
+		var eventName = newOverrideState ? 'd2l-loa-manual-override-enabled' : 'd2l-loa-manual-override-disabled';
+
+		this.dispatchEvent(new CustomEvent(eventName, {
+			bubbles: true
+		}));
 	}
 });
