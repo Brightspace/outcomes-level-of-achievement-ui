fd8694eb14a9e424c04655dd70174d2c4e20fc33
Callum <mitccall@gmail.com>
2020-08-11 16:49:26 -0300

    Clean dead code from calc button

--- calculation/d2l-outcomes-coa-calculate-button.js
+++ calculation/d2l-outcomes-coa-calculate-button.js
@@ -6,6 +6,9 @@ calculated values for a user's overall outcome achievement level
 @demo demo/d2l-outcomes-coa-override-button.html
 */
 
+const KEYCODE_ENTER = 13;
+const KEYCODE_SPACE = 32;
+
 import '@polymer/polymer/polymer-legacy.js';
 import '../localize-behavior.js';
 import 'd2l-polymer-behaviors/d2l-dom.js';
@@ -19,9 +22,7 @@ const $_documentContainer = document.createElement('template');
 
 $_documentContainer.innerHTML = `<dom-module id="d2l-outcomes-coa-calculate-button">
 <template strip-whitespace="">	
-	<style>
-	</style>
-	<d2l-button-icon id="calculateButton" text="[[buttonText]]" icon="[[buttonIcon]]" tabindex="-1">
+	<d2l-button-icon id="calculateButton" text="_getButtonText()" icon="tier1:calculate" tabindex="-1">
 	</d2l-button-icon>
 </template>
 </dom-module> `;
@@ -30,25 +31,6 @@ document.head.appendChild($_documentContainer.content);
 
 Polymer({
 	is: 'd2l-outcomes-coa-calculate-button',
-
-	properties: {
-
-		buttonIcon: {
-			type: String,
-			value: 'tier1:calculate'
-		},
-
-		buttonText: {
-			type: String,
-			value: 'Recalculate Overall Achievement'
-		},
-
-		tooltipPosition: {
-			type: String,
-			value: 'bottom',
-			reflectToAttribute: true
-		},
-	},
 
 	listeners: {
 		'keydown': '_onKeyDown',
@@ -59,19 +41,8 @@ Polymer({
 		D2L.PolymerBehaviors.OutcomesLOA.LocalizeBehavior
 	],
 
-	ready: function() {
-		afterNextRender(this, /* @this */ function() {
-			this.buttonText = this._getButtonText();
-		});
-	},
-
 	_getButtonText: function() {
 		return this.localize('recalculateOverallAchievement');
-	},
-
-	_keyCodes: {
-		ENTER: 13,
-		SPACE: 32
 	},
 
 	_onKeyDown: function(event) {
@@ -79,7 +50,7 @@ Polymer({
 			return;
 		}
 
-		if (event.keyCode === this._keyCodes.ENTER || event.keyCode === this._keyCodes.SPACE) {
+		if (event.keyCode === KEYCODE_ENTER || event.keyCode === KEYCODE_SPACE) {
 			this._handleSelected();
 			event.preventDefault();
 		}
