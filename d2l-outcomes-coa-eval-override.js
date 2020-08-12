e22f8684f43349f4e492b44ad6e95c79fe222fb9
Callum <mitccall@gmail.com>
2020-08-12 14:41:56 -0300

    Merge help, calculate and override buttons into eval-override component

--- d2l-outcomes-coa-eval-override.js
+++ d2l-outcomes-coa-eval-override.js
@@ -8,18 +8,23 @@ import '@polymer/polymer/polymer-legacy.js';
 
 import '@brightspace-ui/core/components/colors/colors.js';
 import '@brightspace-ui/core/components/typography/typography.js';
+import '@brightspace-ui/core/components/button/button.js';
+import '@brightspace-ui/core/components/button/button-icon.js';
+import '@brightspace-ui/core/components/button/button-subtle.js';
+import '@brightspace-ui/core/components/dialog/dialog.js';
+
 import 'd2l-polymer-siren-behaviors/store/entity-store.js';
 import 'd2l-polymer-siren-behaviors/store/entity-behavior.js';
 import 'd2l-polymer-siren-behaviors/store/siren-action-behavior.js';
 
 import './d2l-outcomes-level-of-achievements.js';
-import './override-button/d2l-outcomes-coa-override-button.js';
-import './calculation/d2l-outcomes-coa-calculate-button.js';
-import './calculation/d2l-outcomes-coa-calculation-help.js';
 
 import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
 import './localize-behavior.js';
 const $_documentContainer = document.createElement('template');
+
+const KEYCODE_ENTER = 13;
+const KEYCODE_SPACE = 32;
 
 $_documentContainer.innerHTML = `<dom-module id="d2l-outcomes-coa-eval-override">
 	<template strip-whitespace="">
@@ -89,12 +94,13 @@ $_documentContainer.innerHTML = `<dom-module id="d2l-outcomes-coa-eval-override"
 				margin-bottom: 12px;
 			}
 
-			d2l-outcomes-coa-calculation-help {
+			#help-button {
 				float: left;
 				margin-bottom: 12px;
 				margin-top: 6px;
 				margin-left: 6px;
 			}
+
 			:dir(rtl) d2l-outcomes-coa-calculation-help {
 				float: right;
 				margin-bottom: 12px;
@@ -114,10 +120,6 @@ $_documentContainer.innerHTML = `<dom-module id="d2l-outcomes-coa-eval-override"
 				padding-top: 0px;
 			}
 
-			d2l-outcomes-coa-override-button {
-				
-			}
-
 			:host {
 				display: block;
 			}
@@ -126,7 +128,7 @@ $_documentContainer.innerHTML = `<dom-module id="d2l-outcomes-coa-eval-override"
 			<h3 class="page-heading">Select Overall Achievement</h3>
 			<template is="dom-if" if="[[_isCalculationUpdateNeeded(_calculationMethod, _newAssessmentsAdded, _isOverrideActive)]]">
 				<span class="calculate-button-container">
-					<d2l-outcomes-coa-calculate-button align="right" tabindex="0"></d2l-outcomes-coa-calculate-button>
+					<d2l-button-icon id="calculate-button" onclick="[[_onCalcButtonClicked]]" text="[[localize('recalculateOverallAchievement')]]" icon="tier1:calculate"></d2l-button-icon>
 				</span>
 			</template>
 		</div>
@@ -139,7 +141,7 @@ $_documentContainer.innerHTML = `<dom-module id="d2l-outcomes-coa-eval-override"
 					Calculation method: [[_calculationMethod]]
 				</span>
 				<template is="dom-if" if="[[_hasHelpMenu(_calculationMethod)]]">
-					<d2l-outcomes-coa-calculation-help id="calculation-help" calculation-method="[[_calculationMethod]]" decaying-average-rate="[[_decayingAverageRate]]" tabindex="0"></d2l-outcomes-coa-calculation-help>
+					<d2l-button-icon id="help-button" onclick="[[_onHelpButtonClicked]]" text="[[localize('calculationMethodDetails')]]" icon="tier1:help"></d2l-button-icon>
 				</template>
 			</div>
 		</template>
@@ -152,12 +154,46 @@ $_documentContainer.innerHTML = `<dom-module id="d2l-outcomes-coa-eval-override"
 			</div>
 		</template>
 
-		<d2l-outcomes-level-of-achievements id="level-selector" tooltip-position="top" read-only="[[!_canEditLevel(_isOverrideActive, _calculationMethod)]]" has-calculation="[[_hasCalculation(_calculationMethod)]]" token="[[token]]" href="[[href]]";>
-		</d2l-outcomes-level-of-achievements>
+		<d2l-outcomes-level-of-achievements id="level-selector" tooltip-position="top" read-only="[[!_canEditLevel(_isOverrideActive, _calculationMethod)]]" has-calculation="[[_hasCalculation(_calculationMethod)]]" token="[[token]]" href="[[href]]"></d2l-outcomes-level-of-achievements>
 	
 		<template is="dom-if" if="[[_hasCalculation(_calculationMethod)]]">
-			<d2l-outcomes-coa-override-button id="override-button" tooltip-position="top" override-active="[[_isOverrideActive]]" tabindex="0"></d2l-outcomes-coa-override-button>
+			<d2l-button-subtle id="override-button" onclick="[[_onOverrideButtonClicked]]" text="[[_getOverrideButtonText(_isOverrideActive)]]" icon="[[_getOverrideButtonIcon(_isOverrideActive)]]"></d2l-button-subtle>
 		</template>
+
+		<d2l-dialog id="help-dialog" title-text="[[localize('calculationMethodDetails')]]">
+			<style>
+				p {
+					@apply --d2l-body-text;
+					display: block;
+					content: "";
+					margin-top: 30px;
+				}
+
+				br {
+					display: block;
+					content: "";
+					margin-top: 18px;
+				}
+			</style>
+
+			<div id="help-method">
+				<p><b>[[localize('calcHelpMethodLabel')]]</b><br>[[localize('calcHelpMethodBody', 'calcMethod', _calculationMethod)]]</p>
+			</div>
+			<template is="dom-if" if="[[_shouldHelpShowDecayRate(_calculationMethod)]]" id="help-decaying-rate">
+				<p><b>[[localize('calcHelpDecayRateLabel')]]</b><br>[[localize('calcHelpDecayRateBody', 'number', _decayingAverageRate)]]</p>
+			</template>
+			<div id="help-activities-used">
+				<p><b>[[localize('calcHelpActivitiesLabel')]]</b><br>[[localize('calcHelpActivitiesBody', 'calcActivities', 'All activities')]]</p>
+			</div>
+			<template is="dom-if" if="[[_shouldHelpShowMultipleCommonLevelsPolicy(_calculationMethod)]]" id="help-multi-most-common-policy">
+				<p><b>[[localize('calcHelpMultipleCommonLevelsLabel')]]</b><br>[[localize('calcHelpMultipleCommonLevelsBody', 'policy', 'Highest level')]]</p>
+			</template>
+			<template is="dom-if" if="[[_shouldHelpShowMultipleAttemptsPolicy(_calculationMethod)]]" id="help-multi-attempts-policy">
+				<p><b>[[localize('calcHelpMultipleAttemptsLabel')]]</b><br>[[localize('calcHelpMultipleAttemptsBody', 'policy', 'Highest attempt')]]</p>
+			</template>
+
+			<d2l-button slot="footer" primary data-dialog-action="done">OK</d2l-button>
+		</d2l-dialog>
 	</template>
 
 </dom-module>`;
@@ -178,11 +214,6 @@ Polymer({
 			value: false
 		},
 
-		calculatedLevel: {
-			type: String,
-			value: null
-		},
-
 		_calculationMethod: {
 			type: String,
 			value: null
@@ -198,16 +229,15 @@ Polymer({
 			value: 75
 		},
 
-		_calcUpdateNeeded: {
-			type: Boolean,
-			value: false
-		},
-
-		_loaSelector: {
+		_levelSelector: {
 			type: Object,
 			value: null
 		},
 
+	},
+
+	listeners: {
+		'keydown': '_onKeyDown',
 	},
 
 	observers: [
@@ -221,10 +251,10 @@ Polymer({
 	],
 
 	ready: function() {
+		this._onCalcButtonClicked = this._onCalcButtonClicked.bind(this);
+		this._onOverrideButtonClicked = this._onOverrideButtonClicked.bind(this);
+		this._onHelpButtonClicked = this._onHelpButtonClicked.bind(this);
 		this._levelSelector = this.$$('d2l-outcomes-level-of-achievements');
-		this.addEventListener('d2l-coa-manual-override-enabled', this._onOverrideEnabled);
-		this.addEventListener('d2l-coa-manual-override-disabled', this._onOverrideDisabled);
-		this.addEventListener('d2l-coa-calculation-clicked', this._onCalcButtonClicked);
 	},
 
 	_getCalculationDetails: function(entity) {
@@ -253,7 +283,7 @@ Polymer({
 
 	},
 
-	_isDecayingAverageVisible: function (calculationMethod) {
+	_isDecayingAverageVisible: function(calculationMethod) {
 		return (calculationMethod === 'Decaying Average');
 	},
 
@@ -270,6 +300,25 @@ Polymer({
 			|| calculationMethod === 'Most Common'
 			|| calculationMethod === 'Highest');
 	},
+
+	_shouldHelpShowDecayRate: function (calculationMethod) {
+		return (calculationMethod === 'Decaying Average');
+	},
+
+	_shouldHelpShowMultipleCommonLevelsPolicy: function (calculationMethod) {
+		return (calculationMethod === 'Most Common');
+	},
+
+	_shouldHelpShowMultipleAttemptsPolicy: function(calculationMethod) {
+		switch (calculationMethod) {
+			case 'Decaying Average':
+			case 'Most Common':
+				return true;
+			default:
+				return false;
+		}
+	},
+
 	_canEditLevel: function(overrideActive, calculationMethod) {
 		return overrideActive || !this._hasCalculation(calculationMethod);
 	},
@@ -278,22 +327,52 @@ Polymer({
 		return this.localize('calculatedValue', 'method', calculationMethod, 'value', calculatedValue.toString());
 	},
 
-	_onOverrideEnabled: function() {
-		this._isOverrideActive = true;
-		this._levelSelector.setFocus();
+	_getOverrideButtonText: function(overrideActive) {
+		if (overrideActive) {
+			return this.localize('clearManualOverride');
+		}
+		else {
+			return this.localize('manuallyOverride');
+		}
 	},
 
-	_onOverrideDisabled: function() {
-		//Update the level calculation if necessary
-		if (this._isCalculationUpdateNeeded(this._calculationMethod, this._newAssessmentsAdded, true)) {
-			this._updateLevelCalculation();
+	_getOverrideButtonIcon: function(overrideActive) {
+		if (overrideActive) {
+			return 'tier1:close-default';
 		}
+		else {
+			return 'tier1:edit';
+		}
+	},
 
-		this._levelSelector.resetToSuggested();
-		this._isOverrideActive = false;
+	//For keyboard accessibility
+	_onKeyDown: function(event) {
+		if (event.keyCode === KEYCODE_ENTER || event.keyCode === KEYCODE_SPACE) {
+			console.log(this.shadowRoot.activeElement);
+			this.shadowRoot.activeElement.click();
+			event.preventDefault();
+		}
+	},
+
+	_onOverrideButtonClicked: function() {
+		console.log('override button clicked');
+		console.log(this);
+		if (!this._isOverrideActive) {
+			this._isOverrideActive = true;
+			this._levelSelector.setFocus();
+		}
+		else {
+			if (this._isCalculationUpdateNeeded(this._calculationMethod, this._newAssessmentsAdded, true)) {
+				this._updateLevelCalculation();
+			}
+			this._levelSelector.resetToSuggested();
+			this._isOverrideActive = false;
+		}
 	},
 
 	_onCalcButtonClicked: function() {
+		console.log('calc button clicked');
+		console.log(this);
 		if (this._updateLevelCalculation()) {
 			//Calculation successfully updated
 			this._levelSelector.resetToSuggested();
@@ -301,10 +380,16 @@ Polymer({
 		}
 	},
 
+	_onHelpButtonClicked: function() {
+		var helpDialog = this.shadowRoot.getElementById('help-dialog');
+		if (!helpDialog.opened) {
+			helpDialog.opened = true;
+		}
+	},
+
 	_updateLevelCalculation: function() {
 		//Calculation request will be sent here. This will retrieve a calculated value and any corresponding data
 		this._newAssessmentsAdded = false;
 		return true;
-	}
-
+	},
 });
